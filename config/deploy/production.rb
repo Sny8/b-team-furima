dbaf51922030e6e680b228e0a71ecfe4ebdb9564
Sny8 <65321334+Sny8@users.noreply.github.com>
2020-07-24 22:12:32 +0900

    initial commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '18.178.22.226', user: 'ec2-user', roles: %w{app db web}
+server '54.150.106.241', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
