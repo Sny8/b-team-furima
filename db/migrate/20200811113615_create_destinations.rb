e316937b56d9871b77cb3e10004d1155cb20b525
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-11 20:43:19 +0900

    changed migration file
    destinationテーブルのマイグレーションファイルの編集

--- db/migrate/20200811113615_create_destinations.rb
+++ db/migrate/20200811113615_create_destinations.rb
@@ -1,7 +1,17 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
-
+      t.string     :first_name,       null: false
+      t.string     :family_name,      null: false
+      t.string     :first_name_kana,  null: false
+      t.string     :family_name_kana, null: false
+      t.string     :post_code,        null: false
+      t.integer    :prefecture_code,  null: false
+      t.string     :city,             null: false
+      t.integer    :block,            null: false
+      t.string     :building
+      t.string     :phone_number
+      t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
   end
