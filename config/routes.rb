97d3c314bded9fe41307908b09d4b2f69c01040b
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-13 22:05:02 +0900

    destinations rooting
    destinationsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'destinations', to: 'users/registrations#new_destination'
+    post 'destinations', to: 'users/registrations#create_destination'
+  end
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
