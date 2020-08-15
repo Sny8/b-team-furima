55e6ee931157dc7721e732df03eb2657f897b21f
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-15 13:54:41 +0900

    add JpPrefecture
    prefecture_codeから自動で都道府県名に変更されるようにモデルに追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,15 @@
 class Destination < ApplicationRecord
   belongs_to :user, optional: true
   validates :first_name, :family_name, :first_name_kana, :family_name_kana, :post_code, :prefecture_code, :city, :block, presence: true
+
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture.find(name: prefecture_name).code
+  end
 end
