5aeeaf846ae8545da17b9a31c7fc9dea9965c54e
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-16 14:49:47 +0900

    add user-model validates
    userモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :first_name, :family_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
   has_one :destination
 end
