f1639af8aca27a14ca36b220645befbef793ef31
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-17 22:00:52 +0900

    destination validation
    destinationテーブルのバリデーションの変更
    first_name, family_nameを全角に
    first_name_kana, family_name_kanaを全角ひらがなカタカナに

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,11 @@
 class Destination < ApplicationRecord
   belongs_to :user, optional: true
-  validates :first_name, :family_name, :first_name_kana, :family_name_kana, :post_code, :prefecture_code, :city, :block, presence: true
+  validates :first_name, :family_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
+  validates :post_code,                          presence: true
+  validates :prefecture_code,                    presence: true
+  validates :city,                               presence: true
+  validates :block,                              presence: true
 
   include JpPrefecture
   jp_prefecture :prefecture_code
