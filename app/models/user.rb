da1087789f6bf77e984e4444947045aaf485134c
takuya-nakazato103 <zaxy103@gmail.com>
2020-08-17 21:59:43 +0900

    user validation
    userテーブルのバリデーションの変更
    first_name, family_nameを全角に
    first_name_kana,  family_name_kanaを全角ひらがなカタカナに

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, :family_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name, :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ }
   has_one :destination
 end
